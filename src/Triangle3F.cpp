0883e8e310dadecb9eea47ed321ac5d6ce5edf96
Erwan MATHIEU <erwan.mathieu@ultimaker.com>
2026-09-09 15:25:32 +0200

    Consolidate normal calculation

--- src/Triangle3F.cpp
+++ src/Triangle3F.cpp
@@ -1,5 +1,7 @@
 #include "Triangle3F.h"
 
+#include "geometry_utils.h"
+
 
 Triangle3F::Triangle3F(const Point3F& p1, const Point3F& p2, const Point3F& p3)
     : p1_(p1)
@@ -10,5 +12,5 @@ Triangle3F::Triangle3F(const Point3F& p1, const Point3F& p2, const Point3F& p3)
 
 std::optional<Vector3F> Triangle3F::normal() const
 {
-    return Vector3F(p1_, p2_).cross(Vector3F(p1_, p3_)).normalized();
+    return geometry_utils::triangleNormal(p1_, p2_, p3_);
 }
